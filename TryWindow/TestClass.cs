3603466c3a2662c885e623acb6ce125410bafb2d
Sébastien Geiser <sgeiser@precitrame.com>
2019-06-04 07:29:35 +0200

    For Property access perf compare

---
+++ TryWindow/TestClass.cs
@@ -0,0 +1,20 @@
+﻿namespace TryWindow
+{
+    public class TestClass
+    {
+        public TestClass Child { get; set; }
+
+        public string Text { get; set; } = "Just a text for test";
+
+        public static TestClass Root { get; set; } = new TestClass()
+        {
+            Child = new TestClass()
+            {
+                Child = new TestClass()
+                {
+                    Text = "Last element"
+                }
+            }
+        };
+    }
+}
